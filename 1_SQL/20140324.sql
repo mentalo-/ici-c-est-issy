15694381cb4d4b38eda5059b1b9e91b01e501225
mentalo- <mentalo-@users.noreply.github.com>
2014-09-08 21:44:46 +0200

    Update 20140324.sql

--- 1_SQL/20140324.sql
+++ 1_SQL/20140324.sql
@@ -16,9 +16,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Base de données: `jarodd_issy`
---
 
 -- --------------------------------------------------------
 
@@ -1963,4 +1960,4 @@ ALTER TABLE `poi`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-         
+���������
